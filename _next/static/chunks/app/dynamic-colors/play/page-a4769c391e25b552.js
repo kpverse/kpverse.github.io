8387d7be1863306fa03e0d052c531b2b11a916dc
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2023-09-04 22:32:05 +0530

    Auto-commit by deploy-next-app v0.1.0

---
+++ _next/static/chunks/app/dynamic-colors/play/page-a4769c391e25b552.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[971,400,956],{6039:function(e,n,o){Promise.resolve().then(o.t.bind(o,9118,23))},9118:function(e){e.exports={"iframe-container":"IframeComponent_iframe-container__3MeDZ",iframe:"IframeComponent_iframe__ZsqkU"}}},function(e){e.O(0,[478,596,744],function(){return e(e.s=6039)}),_N_E=e.O()}]);
