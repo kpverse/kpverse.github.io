7b2746398e0910d0cab34284ccd7e9cb868e37d9
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2024-10-28 13:45:27 +0530

    Automated commit by @kpverse's Next JS Deployment Utility (v0.1.2).

---
+++ _next/static/chunks/app/archgemini/feedback/page-da9883c109d5da12.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[898],{3979:function(e,n,o){Promise.resolve().then(o.t.bind(o,7952,23))},7952:function(e){e.exports={"iframe-container":"IframeComponent_iframe-container__3MeDZ",iframe:"IframeComponent_iframe__ZsqkU"}}},function(e){e.O(0,[253,110,744],function(){return e(e.s=3979)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/pages/_error-25469a77921c70e2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{4825:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(6617)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=4825)}),_N_E=n.O()}]);
